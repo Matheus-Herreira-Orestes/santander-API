d4ac42a20f31cb290ad9fb16e6d17b3804ec8a38
Matheus-Herreira-Orestes <mherreirao@gmail.com>
2023-10-20 15:48:07 -0300

    Initial commit

---
+++ src/main/java/me/dio/santanderAPI/SantanderApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santanderAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderApiApplication.class, args);
+	}
+
+}
